c0b52d6299d18751eedb5fdd1d7b8147e955d940
Gustavo Lopez <glopez@emocha.com>
2023-03-12 19:55:28 -0500

    Implement ellens alien game exercise

---
+++ ellens-alien-game/classes.py
@@ -0,0 +1,52 @@
+"""Solution to Ellen's Alien Game exercise."""
+
+
+class Alien:
+    """Create an Alien object with location x_coordinate and y_coordinate.
+
+    Attributes
+    ----------
+    (class)total_aliens_created: int
+    x_coordinate: int - Position on the x-axis.
+    y_coordinate: int - Position on the y-axis.
+    health: int - Amount of health points.
+
+    Methods
+    -------
+    hit(): Decrement Alien health by one point.
+    is_alive(): Return a boolean for if Alien is alive (if health is > 0).
+    teleport(new_x_coordinate, new_y_coordinate): Move Alien object to new coordinates.
+    collision_detection(other): Implementation TBD.
+    """
+
+    total_aliens_created = 0
+
+    def __init__(self, x_coordinate, y_coordinate):
+        self.health = 3
+        Alien.total_aliens_created = Alien.total_aliens_created + 1
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+
+
+    def hit(self):
+        self.health = self.health - 1
+
+
+    def is_alive(self):
+        return self.health > 0
+
+
+    def teleport(self, x_coordinate, y_coordinate):
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+
+
+    def collision_detection(self, param):
+        pass
+
+
+def new_aliens_collection(alien_start_positions):
+    aliens = []
+    for (x_coordinate, y_coordinate) in alien_start_positions:
+        aliens.append(Alien(x_coordinate, y_coordinate))
+    return aliens
