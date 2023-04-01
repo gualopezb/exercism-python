96e664533108b46dd2633e2afb71191616339a7e
Gustavo Lopez <glopez@emocha.com>
2023-04-01 18:07:08 -0500

    Implement resistor-color exercise

---
+++ resistor-color/resistor_color.py
@@ -0,0 +1,36 @@
+def color_code(color):
+    if color == "black":
+        return 0
+    if color == "brown":
+        return 1
+    if color == "red":
+        return 2
+    if color == "orange":
+        return 3
+    if color == "yellow":
+        return 4
+    if color == "green":
+        return 5
+    if color == "blue":
+        return 6
+    if color == "violet":
+        return 7
+    if color == "grey":
+        return 8
+    if color == "white":
+        return 9
+
+
+def colors():
+    return [
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white",
+    ]
